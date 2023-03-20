3857086474fed3909405205036bef7d76f0a097e
paul chau <paul@chau>
2023-03-20 03:06:52 -0400

    fixed the missing & in the http request

--- telegramBot/Telegram.cpp
+++ telegramBot/Telegram.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "Telegram.h"
 #include "Environment.h"
 
@@ -13,6 +14,7 @@ Telegram ::~Telegram () {
 
 
 const CURLcode Telegram::sendMsg(const String& msg) {
-	String httpRequest = API + TG_REQUEST + CHAT_ID + (String)"text=" + msg;
+	String httpRequest = API + TG_SEND_MSG + CHAT_ID + (String)"&text=" + msg;
+	std::cout << httpRequest;
 	return E.setOption(CURLOPT_URL, httpRequest).execute();
 }
