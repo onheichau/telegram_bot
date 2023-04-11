5fa79452c4fb203d4acf32467c7dfa940b18dbe0
paul chau <paul@chau>
2023-04-11 03:18:48 -0400

    updated test item

---
+++ telegramBot/Fincialentity.h
@@ -0,0 +1,18 @@
+#ifndef FINCIALENTITY_H__
+#define FINCIALENTITY_H__
+#include <string>
+
+struct Fincialentity {
+  std::string m_identifier;
+  std::string m_alias;
+  double m_currentPrice{};
+  double m_currentChangePercentage{};
+  double m_previousClose{};
+  int m_holdingPosition;
+  double alertPercentage;
+  bool marketIsOpen{};
+  bool needNotification();
+  std::string createReport();
+};
+
+#endif
