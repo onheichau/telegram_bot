6e97d92d36feb4e627d9a50dc4b33fe2648c0a92
paul chau <paul@chau>
2023-04-19 15:45:51 -0400

    integration test

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_map>
 #include "Timer.h"
 
 using namespace std;
@@ -7,10 +8,10 @@ int main(int argc, char* argv[]) {
   if(argc != 3) {
     cerr << "Invalid argument! do ./executable 'token' 'chatId' <Enter>" << endl;;
   } else {
-    // initialize the Financewatcher
-    Financewatcher watcher("watch.csv", argv[1], argv[2]);
+    // initialize the MarketMonitor
+    MarketMonitor monitor("watch.csv", argv[1], argv[2]);
 
-    timer.loadEvents("event.csv", watcher);
+    timer.registerEvents("singleEvent.csv", monitor.callbackMap());
 
     timer.sortEventList().moveToClosestEvent();
 
@@ -27,6 +28,25 @@ int main(int argc, char* argv[]) {
     for (size_t i = timer.m_currentPosition; i < timer.m_eventListSize; i++) {
       cout << "the up coming event is: " << timer.m_eventsList[i] << endl;
     }
+
+    function<void()>* ptr = timer.m_eventsList[timer.m_currentPosition].m_callback;
+    cout << "the address of the callback stored in event is: " << ptr << endl;
+
+    cout << "extract a pointer from map, then call it: " << endl;
+    function<void()>* callablePtr = addressof(monitor.callbackMap()["UsReport"]);
+    cout << "address of callable ptr is: " << callablePtr << endl;
+    (*callablePtr)();
+
+    cout << "call the function from pointer of call back list" << endl;
+    function<void()>* callbackListEventCallbackPtr = timer.m_eventsList[timer.m_currentPosition].m_callback;
+    cout << "address of the ptr from event list is: " << callbackListEventCallbackPtr << endl;
+    (*callbackListEventCallbackPtr)();
+
+    for(auto it = monitor.callbackMap().begin(); it != monitor.callbackMap().end(); it++) {
+      cout << "the address of the UsReport in map is: " << addressof(it->second) << endl;
+    }
+
+    timer.saveEvents("save.csv", monitor.callbackMap());
   }
   return 0;
 }
