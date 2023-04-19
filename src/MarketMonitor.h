ad72983a1b033c649e60e92f8a2da92af1b97e2f
paul chau <paul@chau>
2023-04-18 22:55:29 -0400

    renamed from watcher

---
+++ src/MarketMonitor.h
@@ -0,0 +1,65 @@
+#ifndef FINANCEWATCHER_H__
+#define FINANCEWATCHER_H__
+#include <iostream>
+#include <string>
+#include "BotIO.h"
+#include "MarketAsset.h"
+
+const int VALID_RESPONSE = 3;
+const int MAX_ATTEMPT = 5;
+
+const std::string previousClosePattern = "PREV_CLOSE-value\">";
+
+class MarketMonitor : public BotIO {
+private:
+  MarketAsset* m_usMarketWatchList{};
+  MarketAsset* m_asiaMarketWatchList{};
+  size_t m_usListSize{};
+  size_t m_asiaListSize{};
+
+  // loads data from file to retrieve the watch list
+  MarketMonitor& load(const std::string& fileName);
+
+  // updates the information of all items in the watch list
+  MarketMonitor& updateWatchList(MarketAsset* watchList, const int size);
+
+  // updates one item's state with latest market information
+  MarketMonitor& updateWatchCase(MarketAsset& watchItem);
+
+  // returns a string that uniquely identify the html element that contains price info
+  std::string latestPricePattern(const std::string& identifier);
+
+  // returns a string that uniquely identify the html element that contains percentage change info
+  std::string changePercentagePattern(const std::string& identifier);
+
+  // extract the double value from plain string that come from html element
+  double extractValue(const std::string& source);
+
+  // extract price info from html element string
+  double extractLatestPrice(const std::string& identifier);
+
+  // extract previous close info from html element string
+  double extractPreviousClose();
+
+  // extract percentage change info from html element string
+  double extractChangePercentage(const std::string& identifier);
+
+public:
+  // constructs the instance's watch list according to the file name provided
+  MarketMonitor(const std::string& fileName, const std::string& token, const std::string& chatId);
+  ~MarketMonitor();
+
+  // 
+  void sendUsMarketDayReport_cb();
+
+  void sendAsiaMarketDayReport_cb();
+
+  void monitorUsMarketPrice_cb();
+
+  void monitorAsiaMarketPrice_cb();
+};
+
+
+
+
+#endif
